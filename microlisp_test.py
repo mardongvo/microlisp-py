382df22dfb11a54651ccc60dc5171c676712765a
mardongvo <mardong_@inbox.ru>
2020-03-24 13:48:15 +0700

    Remake eval for lazy evaluation

--- microlisp_test.py
+++ microlisp_test.py
@@ -36,28 +36,41 @@ class TestMicroLisp(unittest.TestCase):
             ('list',))
     def test_eval1(self):
         expr = microlisp_parse( microlisp_tokenize("(and true false)") )
-        self.assertEqual( microlisp_eval(expr, STANDART_LOGIC_FUNC, {}), False)
+        self.assertEqual( microlisp_eval(STANDART_LOGIC_FUNC, {}, expr), False)
     def test_eval2(self):
         expr = microlisp_parse( microlisp_tokenize("(and (or true false) false)") )
-        self.assertEqual( microlisp_eval(expr, STANDART_LOGIC_FUNC, {}), False)
+        self.assertEqual( microlisp_eval(STANDART_LOGIC_FUNC, {}, expr), False)
     def test_eval3(self):
         expr = microlisp_parse( microlisp_tokenize("(and (or true false) (not false))") )
-        self.assertEqual( microlisp_eval(expr, STANDART_LOGIC_FUNC, {}), True)
+        self.assertEqual( microlisp_eval(STANDART_LOGIC_FUNC, {}, expr), True)
     def test_eval4_exception(self):
         expr = microlisp_parse( microlisp_tokenize("(and (or true false) (booz false))") )
         try:
-            microlisp_eval(expr, STANDART_LOGIC_FUNC, {})
+            microlisp_eval(STANDART_LOGIC_FUNC, {}, expr)
             self.fail("expect exception")
         except RuntimeError:
             self.assertTrue(True)
     def test_eval5_env(self):
         expr = microlisp_parse( microlisp_tokenize("(and (or true false) (not falsekey))") )
-        self.assertEqual( microlisp_eval(expr, STANDART_LOGIC_FUNC, {"falsekey": False}), True)
+        self.assertEqual( microlisp_eval(STANDART_LOGIC_FUNC, {"falsekey": False}, expr), True)
     def test_eval6_testif(self):
         expr = microlisp_parse( microlisp_tokenize("(if true 1 0)") )
-        self.assertEqual( microlisp_eval(expr, STANDART_LOGIC_FUNC, {}), 1)
+        self.assertEqual( microlisp_eval(STANDART_LOGIC_FUNC, {}, expr), 1)
         expr = microlisp_parse( microlisp_tokenize("(if false 1 0)") )
-        self.assertEqual( microlisp_eval(expr, STANDART_LOGIC_FUNC, {}), 0)
+        self.assertEqual( microlisp_eval(STANDART_LOGIC_FUNC, {}, expr), 0)
+    def test_eval7_exception(self):
+        expr = microlisp_parse( microlisp_tokenize("(and (or true false) )") )
+        try:
+            microlisp_eval(STANDART_LOGIC_FUNC, {}, expr)
+            self.fail("expect exception")
+        except RuntimeError:
+            self.assertTrue(True)
+    def test_eval8_env(self):
+        myfuncs = {
+            "+": {"params_count": 2, "func": (lambda funeval, a, b: funeval(a)+funeval(b) )},
+        }
+        expr = microlisp_parse( microlisp_tokenize("(+ (+ 1 2) (+ 2 param))") )
+        self.assertEqual( microlisp_eval(myfuncs, {"param": 3}, expr), 8)
 
 if __name__=='__main__':
 	unittest.main()
